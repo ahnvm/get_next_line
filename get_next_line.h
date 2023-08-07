42b410208d02ca7e55256c9f60e84a2967b7dad6
ahvnm <ahmetabdullahcan@outlook.com>
2023-08-07 23:58:42 +0300

    initial

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acan <ahmetabdullahcan@outlook.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/07 14:15:52 by acan              #+#    #+#             */
+/*   Updated: 2023/08/07 22:23:11 by acan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# include <stdio.h>
+# include <fcntl.h>
+
+int		strlen_int(char *s);
+char	*buf_to_text(char *text, char *buf);
+int		hasnlornull(char *s);
+char	*find_line(int fd, char *text);
+char	*seperate_line(char	*text);
+char	*cut_line(char *text);
+char	*ft_calloc(int count);
+char	*get_next_line(int fd);
+#endif
